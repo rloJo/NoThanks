fada7e818311ff10c7c5a4bedb62beb1814e53a2
rloJo <lew991216@gmail.com>
2023-12-04 23:11:41 +0900

    lobby UI 및 채팅 연결

--- No_Thanks/src/NT_Client/StartPanel.java
+++ No_Thanks/src/NT_Client/StartPanel.java
@@ -6,33 +6,48 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+
+import NT_Client.NTClientView.Myaction;
+
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Container;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class StartPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private Container container;
 	private JFrame mainFrame;
+	private CardLayout cardLayout;
+	private LobbyPanel lobbypanel;
 	private JTextField nameTextField;
 	private helpDialog helpdialog;
+	private JTextField iptextField;
+	private JTextField porttextField;
 
 	/**
 	 * Create the panel.
 	 */
 	public StartPanel(Container container, JFrame mainframe) {
-		setBackground(new Color(225, 113, 0));
+		setBackground(Color.WHITE);
 		this.container = container;
 		this.mainFrame = mainFrame;
+		this.cardLayout = (CardLayout) container.getLayout();
 		setLayout(null);
 		
 		JButton startBtn = new JButton("\uC2DC\uC791\uD558\uAE30");
 		startBtn.setBounds(600, 383, 100, 50);
 		add(startBtn);
+		StartBtnAction startAction = new StartBtnAction();
+		startBtn.addActionListener(startAction);
 		
 		JButton HelpBtn = new JButton("\uB3C4\uC6C0\uB9D0");
 		HelpBtn.setBounds(600, 464, 100, 50);
@@ -56,6 +71,45 @@ public class StartPanel extends JPanel {
 		nickLabel.setFont(new Font("굴림", Font.BOLD, 21));
 		nickLabel.setBounds(375, 329, 198, 45);
 		add(nickLabel);
+		
+		JLabel lblIpAddress = new JLabel("IP Address");
+		lblIpAddress.setBounds(261, 192, 82, 33);
+		add(lblIpAddress);
+		
+		iptextField = new JTextField();
+		iptextField.setText("127.0.0.1");
+		iptextField.setHorizontalAlignment(SwingConstants.CENTER);
+		iptextField.setColumns(10);
+		iptextField.setBounds(382, 198, 116, 33);
+		add(iptextField);
+		
+		JLabel lblPortNumber = new JLabel("Port Number");
+		lblPortNumber.setBounds(261, 261, 82, 33);
+		add(lblPortNumber);
+		
+		porttextField = new JTextField();
+		porttextField.setText("30000");
+		porttextField.setHorizontalAlignment(SwingConstants.CENTER);
+		porttextField.setColumns(10);
+		porttextField.setBounds(382, 261, 116, 33);
+		add(porttextField);
+	}
+	
+	class StartBtnAction implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String userName = nameTextField.getText().trim();
+			if(userName.equals("")) { // 이름 미입력 시
+				userName = "#" + Integer.toString(user.get_userCount()) + " user";
+			}
+		
+			String ip_addr = iptextField.getText().trim();
+			String port_no = porttextField.getText().trim();
+			
+			lobbypanel = new LobbyPanel(container, mainFrame, userName, ip_addr, port_no);
+			container.add(lobbypanel, "lobbypanel");
+			cardLayout.show(container, "lobbypanel"); // 대기실 Panel로 변경
+		}
 	}
 	
 	@Override
@@ -65,6 +119,4 @@ public class StartPanel extends JPanel {
 		ImageIcon NTbgImg = new ImageIcon("images\\NT_bg.jpg");
 		g.drawImage(NTbgImg.getImage(), 0, 0, 400,600,this);
 	}
-
-	
 }
---
+++ No_Thanks/src/NT_Client/user.java
@@ -0,0 +1,51 @@
+package NT_Client;
+
+import java.util.ArrayList;
+
+class user {
+	private static int user_count = 1; // default 유저 이름을 지어주기 위한 변수
+	private String name;			   // 유저 이름
+	private int token;				   // 게임 진행에 필요한 유저가 갖는 토큰
+	private ArrayList<Integer> cards;  // 유저가 획득한 카드
+	
+	public user() {
+		name = "#" + user_count + " user";
+		token = 11;
+		cards = new ArrayList<Integer>();
+		user_count++;
+	}
+	
+	public user(String u_name) {
+		//user_count++;
+		name = u_name;
+		token = 11;
+		cards = new ArrayList<Integer>();
+	}
+	
+	public static int get_userCount(){
+		return user_count;
+	}
+	
+	public void set_token(int game_token) {
+		this.token += game_token;
+	}
+	
+	public int get_token() {	
+		return this.token;
+	}
+	
+	public String get_name() {	
+		return this.name;
+	}
+	
+	public void set_cards(int card_num) {
+		cards.add(card_num);
+		// 승자를 가리기위한 정렬 ?
+	}
+	
+	public ArrayList<Integer> get_cards() {
+		return this.cards;
+	}
+	
+}
+
